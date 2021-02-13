5435c61c9ccbbed0fe4f2935ee308761e49eb891
SorinEni <60381859+SorinEni@users.noreply.github.com>
2021-02-13 13:08:22 +0100

    Add files via upload

---
+++ classrooms.sql
@@ -0,0 +1,79 @@
+CREATE TABLE classroom (
+id_classroom int AUTO_INCREMENT,
+number text,
+description text,
+building INT,
+seats_count int,
+is_computers_lab tinyint
+);
+
+
+INSERT INTO classroom
+VALUES(
+    null, "6A", "nevim", "1", "32", "0"
+);
+
+
+INSERT INTO classroom
+VALUES(
+    null, "6B", "nevim", "2", "33", "0"
+);
+
+
+INSERT INTO classroom
+VALUES(
+    null, "6H", "nevim", "3", "43", "1"
+);
+
+
+INSERT INTO classroom
+VALUES(
+    null, "3A", "nevim", "1", "23", "1"
+);
+
+INSERT INTO classroom
+VALUES(
+    null, "5A", "nevim", "1", "31", "1"
+);
+
+
+INSERT INTO classroom
+VALUES(
+    null, "3B", "nevim", "2", "31", "0"
+);
+
+
+INSERT INTO classroom
+VALUES(
+    null, "4B", "nevim", "2", "33", "0"
+);
+
+
+INSERT INTO classroom
+VALUES(
+    null, "9B", "nevim", "2", "33", "0"
+);
+
+
+INSERT INTO classroom
+VALUES(
+    null, "9C", "nevim", "3", "33", "0"
+);
+
+
+INSERT INTO classroom
+VALUES(
+    null, "8C", "nevim", "3", "11", "1"
+);
+
+
+INSERT INTO classroom
+VALUES(
+    null, "7C", "nevim", "3", "11", "1"
+);
+
+
+INSERT INTO classroom
+VALUES(
+    null, "6C", "nevim", "3", "11", "1"
+);

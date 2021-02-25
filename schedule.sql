c7cb3f2a7ca3f4fd49e80811518f2916b0c3d250
SorinEni <60381859+SorinEni@users.noreply.github.com>
2021-02-25 09:20:29 +0100

    Update schedule.sql

--- schedule.sql
+++ schedule.sql
@@ -20,3 +20,31 @@ day_of_week tinyint);
 
 INSERT INTO schedule (id_schedule, id_classroom, id_teacher, id_subject, lesson_number, day_of_week)
 VALUES ("1", "1", "1", "1", "1", "1")
+
+INSERT INTO schedule (id_schedule, id_classroom, id_teacher, id_subject, lesson_number, day_of_week)
+VALUES ("2", "2", "2", "3", "2", "3")
+
+INSERT INTO schedule (id_schedule, id_classroom, id_teacher, id_subject, lesson_number, day_of_week)
+VALUES ("3", "1", "3", "2", "3", "5")
+
+INSERT INTO schedule (id_schedule, id_classroom, id_teacher, id_subject, lesson_number, day_of_week)
+VALUES ("4", "3", "1", "1", "4", "4")
+
+INSERT INTO schedule (id_schedule, id_classroom, id_teacher, id_subject, lesson_number, day_of_week)
+VALUES ("5", "5", "2", "3", "5", "5")
+
+INSERT INTO schedule (id_schedule, id_classroom, id_teacher, id_subject, lesson_number, day_of_week)
+VALUES ("6", "4", "3", "2", "6", "4")
+
+INSERT INTO schedule (id_schedule, id_classroom, id_teacher, id_subject, lesson_number, day_of_week)
+VALUES ("7", "3", "1", "1", "7", "3")
+
+INSERT INTO schedule (id_schedule, id_classroom, id_teacher, id_subject, lesson_number, day_of_week)
+VALUES ("8", "5", "2", "3", "8", "2")
+
+INSERT INTO schedule (id_schedule, id_classroom, id_teacher, id_subject, lesson_number, day_of_week)
+VALUES ("9", "2", "3", "2", "9", "1")
+
+INSERT INTO schedule (id_schedule, id_classroom, id_teacher, id_subject, lesson_number, day_of_week)
+VALUES ("10", "4", "3", "1", "9", "1")
+
